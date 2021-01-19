0165abd0b819949756db0230e6182ff4b807dd03
Carol <oncwnuDS7X6h9LpPxLZ0VrVKcPkU@git.weixin.qq.com>
2021-01-19 11:02:01 +0800

    first commit

---
+++ data/data.js
@@ -0,0 +1,453 @@
+var detailData = [
+  {
+    title: "黄土",
+    enTitle: "Huanɡ Tu",
+    img: "./images/detail/1.jpg",
+    des: "黄土是指在地质时代中的第四纪期间，以风力搬运的黄色粉土沉积物。它是原生的、成厚层连续分布，掩覆在低分水岭、山坡、丘陵，常与基岩不整合接触，无层理，常含有古土壤层及钙质结核层，垂直节理发育，常形成陡壁。",
+    XMLConfig: {
+      path: "./images/st1/",
+      filePrefix: "st1.00"
+    },
+    stopIndex: 0
+  },
+  {
+    title: "复成分砾岩",
+    enTitle: "Fu Cheng Fen Li Yan",
+    img: "./images/detail/2.jpg",
+    des: "复成分砾岩(角砾岩) 砾石成分复杂，各种成分的砾石含量都不超过50%，其中大部分砾石抗风化能力不强，常分选不好、圆度不高。这种砾岩多沿山麓呈带状分布，为古老山脉中的母岩迅速破坏、迅速堆积的产物。这种砾岩的成因可以很多，但以造山后期的冲积扇和河流沉积的最为常见。",
+    XMLConfig: {
+      path: "./images/st2/",
+      filePrefix: "st2.00"
+    },
+    stopIndex: 1
+  },
+  {
+    title: "含油砂岩",
+    enTitle: "Han You Sha Yan",
+    img: "./images/detail/3.jpg",
+    des: "浅灰色，砂状结构，块状构造，主要矿物：石英、长石碎屑，层理不发育，中细颗粒。",
+    XMLConfig: {
+      path: "./images/st3/",
+      filePrefix: "st3.00"
+    },
+    stopIndex: 2
+  },
+  {
+    title: "泥岩",
+    enTitle: "Ni Yan",
+    img: "./images/detail/4.jpg",
+    des: "泥岩是指弱固结的黏土经过中等程度的后生作用（如挤压作用、脱水作用、重结晶作用和胶结作用）形成强固结的岩石。是已固结成岩的，但层理不明显，或呈块状，局部失去可塑性，遇水不立即膨胀的沉积型岩石。",
+    XMLConfig: {
+      path: "./images/st4/",
+      filePrefix: "st4.00"
+    },
+    stopIndex: 3
+  },
+  {
+    title: "膏岩",
+    enTitle: "Gao Yan",
+    img: "./images/detail/5.jpg",
+    des: "石膏岩，多为高度浓缩海水环境（如萨勃哈环境或泻湖）沉积物的成岩产物，主要成分为石膏，黑灰色。",
+    XMLConfig: {
+      path: "./images/st5/",
+      filePrefix: "st5.00"
+    },
+    stopIndex: 4
+  },
+  {
+    title: "复成分砾岩",
+    enTitle: "Fu Cheng Fen Li Yan",
+    img: "./images/detail/6.jpg",
+    des: "复成分砾岩(角砾岩) 砾石成分复杂，各种成分的砾石含量都不超过50%，其中大部分砾石抗风化能力不强，常分选不好、圆度不高。这种砾岩多沿山麓呈带状分布，为古老山脉中的母岩迅速破坏、迅速堆积的产物。这种砾岩的成因可以很多，但以造山后期的冲积扇和河流沉积的最为常见。",
+    XMLConfig: {
+      path: "./images/st6/",
+      filePrefix: "st6.00"
+    },
+    stopIndex: 5
+  },
+  {
+    title: "砂岩",
+    enTitle: "Sha Yan",
+    img: "./images/detail/7.jpg",
+    des: "砂岩是一种沉积岩，主要由各种砂粒胶结而成的，颗粒直径在0.05-2mm，其中砂粒含量要大于50%，结构稳定，通常呈淡褐色或红色，主要含硅、钙、黏土和氧化铁。绝大部分砂岩是由石英或长石组成的。",
+    XMLConfig: {
+      path: "./images/st7/",
+      filePrefix: "st7.00"
+    },
+    stopIndex: 6
+  },
+  {
+    title: "复成分砾岩",
+    enTitle: "Fu Cheng Fen Li Yan",
+    img: "./images/detail/8.jpg",
+    des: "复成分砾岩(角砾岩) 砾石成分复杂，各种成分的砾石含量都不超过50%，其中大部分砾石抗风化能力不强，常分选不好、圆度不高。这种砾岩多沿山麓呈带状分布，为古老山脉中的母岩迅速破坏、迅速堆积的产物。这种砾岩的成因可以很多，但以造山后期的冲积扇和河流沉积的最为常见。",
+    XMLConfig: {
+      path: "./images/st8/",
+      filePrefix: "st8.00"
+    },
+    stopIndex: 7
+  },
+  {
+    title: "煤岩",
+    enTitle: "Mei Yan",
+    img: "./images/detail/9.jpg",
+    des: "如煤岩组分、矿物质的含量、风化程度以及挤压错动和破碎程度等，特别是煤的光泽强度随变质程度的加深而增强。例如高变质半暗煤的光泽强度往往强于低变质半亮煤的光泽强度，因此划分煤岩类型应以变质程度相同的煤做比较。",
+    XMLConfig: {
+      path: "./images/st9/",
+      filePrefix: "st9.00"
+    },
+    stopIndex: 8
+  },
+  {
+    title: "砂岩",
+    enTitle: "Sha Yan",
+    img: "./images/detail/10.jpg",
+    des: "砂岩是一种沉积岩，主要由各种砂粒胶结而成的，颗粒直径在0.05-2mm，其中砂粒含量要大于50%，结构稳定，通常呈淡褐色或红色，主要含硅、钙、黏土和氧化铁。绝大部分砂岩是由石英或长石组成的。",
+    XMLConfig: {
+      path: "./images/st10/",
+      filePrefix: "st10.00"
+    },
+    stopIndex: 9
+  },
+  {
+    title: "流纹岩",
+    enTitle: "Liu Wen Yan",
+    img: "./images/detail/11.jpg",
+    des: "流纹岩是一种火成岩，是火山的酸性喷出岩石，其化学成分与花岗岩相同，由于形成时冷却速度较快使矿物来不及结晶，二氧化硅含量大于69%，其斑晶主要为钾长石和石英组成，晶体形状为方形板状，有玻璃光泽，但有节理。岩石为灰色、粉红色或砖红色，有流纹构造和斑状结构。流纹岩是一类SiO2含量大于65%、富含长石石英矿物的酸性喷出岩，在全球广泛分布。",
+    XMLConfig: {
+      path: "./images/st11/",
+      filePrefix: "st11.00"
+    },
+    stopIndex: 10
+  },
+  {
+    title: "长石石英砂岩",
+    enTitle: "Chang Shi Dan Ying Sha Yan",
+    img: "./images/detail/12.jpg",
+    des: "长石石英砂岩，当砂粒中含较多量的石英碎屑（石英大于75%）时，即过渡为长石石英砂岩（feldspathic quartz sandstone），或称次长石砂岩（subarkose）。",
+    XMLConfig: {
+      path: "./images/st12/",
+      filePrefix: "st12.00"
+    },
+    stopIndex: 11
+  },
+  {
+    title: "长石石英砂岩",
+    enTitle: "Chang Shi Dan Ying Sha Yan",
+    img: "./images/detail/13.jpg",
+    des: "黄土是指在地质时代中的第四纪期间，以风力搬运的黄色粉土沉积物。它是原生的、成厚层连续分布，掩覆在低分水岭、山坡、丘陵，常与基岩不整合接触，无层理，常含有古土壤层及钙质结核层，垂直节理发育，常形成陡壁。",
+    XMLConfig: {
+      path: "./images/st1/",
+      filePrefix: "st1.00"
+    },
+    stopIndex: 12
+  },
+  {
+    title: "生物碎屑灰岩",
+    enTitle: "Shenɡ Wu Sui Xie Hui Yan",
+    img: "./images/detail/14.jpg",
+    des: "复成分砾岩(角砾岩) 砾石成分复杂，各种成分的砾石含量都不超过50%，其中大部分砾石抗风化能力不强，常分选不好、圆度不高。这种砾岩多沿山麓呈带状分布，为古老山脉中的母岩迅速破坏、迅速堆积的产物。这种砾岩的成因可以很多，但以造山后期的冲积扇和河流沉积的最为常见。",
+    XMLConfig: {
+      path: "./images/st2/",
+      filePrefix: "st2.00"
+    },
+    stopIndex: 13
+  },
+  {
+    title: "煤岩",
+    enTitle: "Mei Yan",
+    img: "./images/detail/15.jpg",
+    des: "浅灰色，砂状结构，块状构造，主要矿物：石英、长石碎屑，层理不发育，中细颗粒。",
+    XMLConfig: {
+      path: "./images/st3/",
+      filePrefix: "st3.00"
+    },
+    stopIndex: 14
+  },
+  {
+    title: "灰岩",
+    enTitle: "Hui Yan",
+    img: "./images/detail/16.jpg",
+    des: "泥岩是指弱固结的黏土经过中等程度的后生作用（如挤压作用、脱水作用、重结晶作用和胶结作用）形成强固结的岩石。是已固结成岩的，但层理不明显，或呈块状，局部失去可塑性，遇水不立即膨胀的沉积型岩石。",
+    XMLConfig: {
+      path: "./images/st4/",
+      filePrefix: "st4.00"
+    },
+    stopIndex: 15
+  },
+  {
+    title: "砾岩",
+    enTitle: "Li Yan",
+    img: "./images/detail/17.jpg",
+    des: "石膏岩，多为高度浓缩海水环境（如萨勃哈环境或泻湖）沉积物的成岩产物，主要成分为石膏，黑灰色。",
+    XMLConfig: {
+      path: "./images/st5/",
+      filePrefix: "st5.00"
+    },
+    stopIndex: 16
+  },
+  {
+    title: "火山集块岩",
+    enTitle: "Huo Shan Ji Kuai Yan",
+    img: "./images/detail/18.jpg",
+    des: "复成分砾岩(角砾岩) 砾石成分复杂，各种成分的砾石含量都不超过50%，其中大部分砾石抗风化能力不强，常分选不好、圆度不高。这种砾岩多沿山麓呈带状分布，为古老山脉中的母岩迅速破坏、迅速堆积的产物。这种砾岩的成因可以很多，但以造山后期的冲积扇和河流沉积的最为常见。",
+    XMLConfig: {
+      path: "./images/st6/",
+      filePrefix: "st6.00"
+    },
+    stopIndex: 17
+  },
+  {
+    title: "长石石英砂岩",
+    enTitle: "Chang Shi Dan Ying Sha Yan",
+    img: "./images/detail/19.jpg",
+    des: "砂岩是一种沉积岩，主要由各种砂粒胶结而成的，颗粒直径在0.05-2mm，其中砂粒含量要大于50%，结构稳定，通常呈淡褐色或红色，主要含硅、钙、黏土和氧化铁。绝大部分砂岩是由石英或长石组成的。",
+    XMLConfig: {
+      path: "./images/st7/",
+      filePrefix: "st7.00"
+    },
+    stopIndex: 18
+  },
+  {
+    title: "砂岩",
+    enTitle: "Sha Yan",
+    img: "./images/detail/20.jpg",
+    des: "复成分砾岩(角砾岩) 砾石成分复杂，各种成分的砾石含量都不超过50%，其中大部分砾石抗风化能力不强，常分选不好、圆度不高。这种砾岩多沿山麓呈带状分布，为古老山脉中的母岩迅速破坏、迅速堆积的产物。这种砾岩的成因可以很多，但以造山后期的冲积扇和河流沉积的最为常见。",
+    XMLConfig: {
+      path: "./images/st8/",
+      filePrefix: "st8.00"
+    },
+    stopIndex: 19
+  },
+  {
+    title: "安山岩",
+    enTitle: "An Shan Yan",
+    img: "./images/detail/21.jpg",
+    des: "如煤岩组分、矿物质的含量、风化程度以及挤压错动和破碎程度等，特别是煤的光泽强度随变质程度的加深而增强。例如高变质半暗煤的光泽强度往往强于低变质半亮煤的光泽强度，因此划分煤岩类型应以变质程度相同的煤做比较。",
+    XMLConfig: {
+      path: "./images/st9/",
+      filePrefix: "st9.00"
+    },
+    stopIndex: 20
+  },
+  {
+    title: "杏仁状玄武岩",
+    enTitle: "Xing Ren Zhuang Xuan Wu Yan",
+    img: "./images/detail/22.jpg",
+    des: "砂岩是一种沉积岩，主要由各种砂粒胶结而成的，颗粒直径在0.05-2mm，其中砂粒含量要大于50%，结构稳定，通常呈淡褐色或红色，主要含硅、钙、黏土和氧化铁。绝大部分砂岩是由石英或长石组成的。",
+    XMLConfig: {
+      path: "./images/st10/",
+      filePrefix: "st10.00"
+    },
+    stopIndex: 21
+  },
+  {
+    title: "灰岩",
+    enTitle: "Hui Yan",
+    img: "./images/detail/23.jpg",
+    des: "流纹岩是一种火成岩，是火山的酸性喷出岩石，其化学成分与花岗岩相同，由于形成时冷却速度较快使矿物来不及结晶，二氧化硅含量大于69%，其斑晶主要为钾长石和石英组成，晶体形状为方形板状，有玻璃光泽，但有节理。岩石为灰色、粉红色或砖红色，有流纹构造和斑状结构。流纹岩是一类SiO2含量大于65%、富含长石石英矿物的酸性喷出岩，在全球广泛分布。",
+    XMLConfig: {
+      path: "./images/st11/",
+      filePrefix: "st11.00"
+    },
+    stopIndex: 22
+  },
+  {
+    title: "安山岩",
+    enTitle: "An Shan Yan",
+    img: "./images/detail/24.jpg",
+    des: "长石石英砂岩，当砂粒中含较多量的石英碎屑（石英大于75%）时，即过渡为长石石英砂岩（feldspathic quartz sandstone），或称次长石砂岩（subarkose）。",
+    XMLConfig: {
+      path: "./images/st12/",
+      filePrefix: "st12.00"
+    },
+    stopIndex: 23
+  },
+  {
+    title: "长石石英砂岩",
+    enTitle: "Chang Shi Dan Ying Sha Yan",
+    img: "./images/detail/25.jpg",
+    des: "黄土是指在地质时代中的第四纪期间，以风力搬运的黄色粉土沉积物。它是原生的、成厚层连续分布，掩覆在低分水岭、山坡、丘陵，常与基岩不整合接触，无层理，常含有古土壤层及钙质结核层，垂直节理发育，常形成陡壁。",
+    XMLConfig: {
+      path: "./images/st1/",
+      filePrefix: "st1.00"
+    },
+    stopIndex: 24
+  },
+  {
+    title: "冰碛砾岩",
+    enTitle: "Bing Qi Li Yan",
+    img: "./images/detail/26.jpg",
+    des: "复成分砾岩(角砾岩) 砾石成分复杂，各种成分的砾石含量都不超过50%，其中大部分砾石抗风化能力不强，常分选不好、圆度不高。这种砾岩多沿山麓呈带状分布，为古老山脉中的母岩迅速破坏、迅速堆积的产物。这种砾岩的成因可以很多，但以造山后期的冲积扇和河流沉积的最为常见。",
+    XMLConfig: {
+      path: "./images/st2/",
+      filePrefix: "st2.00"
+    },
+    stopIndex: 25
+  },
+  {
+    title: "石英砂岩",
+    enTitle: "Dan Ying Sha Yan",
+    img: "./images/detail/27.jpg",
+    des: "浅灰色，砂状结构，块状构造，主要矿物：石英、长石碎屑，层理不发育，中细颗粒。",
+    XMLConfig: {
+      path: "./images/st3/",
+      filePrefix: "st3.00"
+    },
+    stopIndex: 26
+  },
+  {
+    title: "变砂岩",
+    enTitle: "Bian Sha Yan",
+    img: "./images/detail/28.jpg",
+    des: "泥岩是指弱固结的黏土经过中等程度的后生作用（如挤压作用、脱水作用、重结晶作用和胶结作用）形成强固结的岩石。是已固结成岩的，但层理不明显，或呈块状，局部失去可塑性，遇水不立即膨胀的沉积型岩石。",
+    XMLConfig: {
+      path: "./images/st4/",
+      filePrefix: "st4.00"
+    },
+    stopIndex: 27
+  },
+  {
+    title: "绢云石英片岩",
+    enTitle: "Juan Yun Shi Ying Pian Yan",
+    img: "./images/detail/29.jpg",
+    des: "石膏岩，多为高度浓缩海水环境（如萨勃哈环境或泻湖）沉积物的成岩产物，主要成分为石膏，黑灰色。",
+    XMLConfig: {
+      path: "./images/st5/",
+      filePrefix: "st5.00"
+    },
+    stopIndex: 28
+  },
+  {
+    title: "灰岩",
+    enTitle: "Hui Yan",
+    img: "./images/detail/30.jpg",
+    des: "复成分砾岩(角砾岩) 砾石成分复杂，各种成分的砾石含量都不超过50%，其中大部分砾石抗风化能力不强，常分选不好、圆度不高。这种砾岩多沿山麓呈带状分布，为古老山脉中的母岩迅速破坏、迅速堆积的产物。这种砾岩的成因可以很多，但以造山后期的冲积扇和河流沉积的最为常见。",
+    XMLConfig: {
+      path: "./images/st6/",
+      filePrefix: "st6.00"
+    },
+    stopIndex: 29
+  },
+  {
+    title: "白云岩",
+    enTitle: "Bai Yun Yan",
+    img: "./images/detail/31.jpg",
+    des: "砂岩是一种沉积岩，主要由各种砂粒胶结而成的，颗粒直径在0.05-2mm，其中砂粒含量要大于50%，结构稳定，通常呈淡褐色或红色，主要含硅、钙、黏土和氧化铁。绝大部分砂岩是由石英或长石组成的。",
+    XMLConfig: {
+      path: "./images/st7/",
+      filePrefix: "st7.00"
+    },
+    stopIndex: 30
+  },
+  {
+    title: "黑云石英片岩",
+    enTitle: "Hei Yun Shi Ying Pian Yan",
+    img: "./images/detail/32.jpg",
+    des: "复成分砾岩(角砾岩) 砾石成分复杂，各种成分的砾石含量都不超过50%，其中大部分砾石抗风化能力不强，常分选不好、圆度不高。这种砾岩多沿山麓呈带状分布，为古老山脉中的母岩迅速破坏、迅速堆积的产物。这种砾岩的成因可以很多，但以造山后期的冲积扇和河流沉积的最为常见。",
+    XMLConfig: {
+      path: "./images/st8/",
+      filePrefix: "st8.00"
+    },
+    stopIndex: 31
+  },
+  {
+    title: "条带状灰岩",
+    enTitle: "Tiao Dai Zhuang Hui Yan",
+    img: "./images/detail/33.jpg",
+    des: "如煤岩组分、矿物质的含量、风化程度以及挤压错动和破碎程度等，特别是煤的光泽强度随变质程度的加深而增强。例如高变质半暗煤的光泽强度往往强于低变质半亮煤的光泽强度，因此划分煤岩类型应以变质程度相同的煤做比较。",
+    XMLConfig: {
+      path: "./images/st9/",
+      filePrefix: "st9.00"
+    },
+    stopIndex: 32
+  },
+  {
+    title: "千枚岩",
+    enTitle: "Qian Mei Yan",
+    img: "./images/detail/34.jpg",
+    des: "砂岩是一种沉积岩，主要由各种砂粒胶结而成的，颗粒直径在0.05-2mm，其中砂粒含量要大于50%，结构稳定，通常呈淡褐色或红色，主要含硅、钙、黏土和氧化铁。绝大部分砂岩是由石英或长石组成的。",
+    XMLConfig: {
+      path: "./images/st10/",
+      filePrefix: "st10.00"
+    },
+    stopIndex: 33
+  },
+  {
+    title: "石英岩",
+    enTitle: "Shi Yinɡ Yan",
+    img: "./images/detail/35.jpg",
+    des: "流纹岩是一种火成岩，是火山的酸性喷出岩石，其化学成分与花岗岩相同，由于形成时冷却速度较快使矿物来不及结晶，二氧化硅含量大于69%，其斑晶主要为钾长石和石英组成，晶体形状为方形板状，有玻璃光泽，但有节理。岩石为灰色、粉红色或砖红色，有流纹构造和斑状结构。流纹岩是一类SiO2含量大于65%、富含长石石英矿物的酸性喷出岩，在全球广泛分布。",
+    XMLConfig: {
+      path: "./images/st11/",
+      filePrefix: "st11.00"
+    },
+    stopIndex: 34
+  },
+  {
+    title: "黑云斜长片麻岩",
+    enTitle: "Hei Yun Xie Chang Pian Ma Yan",
+    img: "./images/detail/36.jpg",
+    des: "长石石英砂岩，当砂粒中含较多量的石英碎屑（石英大于75%）时，即过渡为长石石英砂岩（feldspathic quartz sandstone），或称次长石砂岩（subarkose）。",
+    XMLConfig: {
+      path: "./images/st12/",
+      filePrefix: "st12.00"
+    },
+    stopIndex: 35
+  },
+  {
+    title: "石英片岩",
+    enTitle: "Shi Ying Pian Yan",
+    img: "./images/detail/37.jpg",
+    des: "黄土是指在地质时代中的第四纪期间，以风力搬运的黄色粉土沉积物。它是原生的、成厚层连续分布，掩覆在低分水岭、山坡、丘陵，常与基岩不整合接触，无层理，常含有古土壤层及钙质结核层，垂直节理发育，常形成陡壁。",
+    XMLConfig: {
+      path: "./images/st1/",
+      filePrefix: "st1.00"
+    },
+    stopIndex: 36
+  },
+  {
+    title: "斜长角闪岩",
+    enTitle: "Xie Chang Jiao Shan Yan",
+    img: "./images/detail/38.jpg",
+    des: "复成分砾岩(角砾岩) 砾石成分复杂，各种成分的砾石含量都不超过50%，其中大部分砾石抗风化能力不强，常分选不好、圆度不高。这种砾岩多沿山麓呈带状分布，为古老山脉中的母岩迅速破坏、迅速堆积的产物。这种砾岩的成因可以很多，但以造山后期的冲积扇和河流沉积的最为常见。",
+    XMLConfig: {
+      path: "./images/st2/",
+      filePrefix: "st2.00"
+    },
+    stopIndex: 37
+  },
+  {
+    title: "大理岩",
+    enTitle: "Da Li Yan",
+    img: "./images/detail/39.jpg",
+    des: "浅灰色，砂状结构，块状构造，主要矿物：石英、长石碎屑，层理不发育，中细颗粒。",
+    XMLConfig: {
+      path: "./images/st3/",
+      filePrefix: "st3.00"
+    },
+    stopIndex: 38
+  },
+  {
+    title: "麻粒岩",
+    enTitle: "Ma Li Yan",
+    img: "./images/detail/40.jpg",
+    des: "泥岩是指弱固结的黏土经过中等程度的后生作用（如挤压作用、脱水作用、重结晶作用和胶结作用）形成强固结的岩石。是已固结成岩的，但层理不明显，或呈块状，局部失去可塑性，遇水不立即膨胀的沉积型岩石。",
+    XMLConfig: {
+      path: "./images/st4/",
+      filePrefix: "st4.00"
+    },
+    stopIndex: 39
+  },
+  {
+    title: "花岗闪长片麻岩",
+    enTitle: "Hua Gang Shan Chang Pian Ma Yan",
+    img: "./images/detail/41.jpg",
+    des: "石膏岩，多为高度浓缩海水环境（如萨勃哈环境或泻湖）沉积物的成岩产物，主要成分为石膏，黑灰色。",
+    XMLConfig: {
+      path: "./images/st5/",
+      filePrefix: "st5.00"
+    },
+    stopIndex: 40
+  }
+]
